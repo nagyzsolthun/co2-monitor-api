a9e877c1f235152e454b24109e055ae54085bddf
Zsolt Nagy <nagy.zsolt.hun@gmail.com>
2022-03-14 18:55:08 +0100

    spring initializr

---
+++ src/main/java/hu/zsoltn/co2/Co2MonitorApiApplication.java
@@ -0,0 +1,13 @@
+package hu.zsoltn.co2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Co2MonitorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Co2MonitorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/zsoltn/co2/Co2MonitorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.zsoltn.co2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Co2MonitorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
